6b0f06fff0458c20ff0813d74e3ec6305a8b8e33
lihhhan <lh_dlut@163.com>
2016-05-19 19:36:11 +0800

    111

--- weather_classify/app.py
+++ weather_classify/app.py
@@ -2,7 +2,7 @@
 import feature,  video
 import sys, os, logging
 import numpy as np
-
+from sqlite3 import dbapi2 as sqlite3
 
 logging.basicConfig(level=logging.DEBUG,
     format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
@@ -10,12 +10,20 @@ logging.basicConfig(level=logging.DEBUG,
     filename='./log/classify.log',
     filemode='w')
 
-if len(sys.argv) > 2:
+if len(sys.argv) > 3:
     name = sys.argv[1]
     out = sys.argv[2]
+    fname = sys.argv[3]
 else :
     exit();
 v = video.video(name)
 v.run(out)
 
+
+pwd = os.getcwd()
+rv = sqlite3.connect(pwd + '/tasklist.db')
+
+rv.execute('update tasks set prograss = "1" where filename = "%s"' %fname)
+rv.commit()
+rv.close()
 
